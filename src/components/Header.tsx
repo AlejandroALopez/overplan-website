e609bd62ef1eb1ac39c82ae271a18f6c171d5df8
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-06-24 11:16:43 -0400

    header updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,25 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="fixed z-20 flex flex-row w-full items-center justify-between text-center gap-16 py-3 px-4 bg-white">
+        <div className="fixed z-20 flex flex-row w-full items-center justify-between text-center gap-16 py-3 px-6 bg-white">
             {/* Logo */}
-            <Link href="/">
-                <p className="text-2xl md:text-3xl font-semibold transition hover:scale-110 duration-300">OverPlan AI</p>
-            </Link>
+            <div>
+                <p className="text-2xl md:text-3xl font-semibold">OverPlan AI</p>
+            </div>
+            {/* Sign In and Try Now buttons */}
+            <div className="flex flex-row gap-8 items-center">
+                <button
+                    className="transition hover:scale-105 duration-300"
+                >
+                    <p>Sign in</p>
+                </button>
+                <button
+                    className="flex items-center justify-center bg-primary rounded-lg px-4 py-2
+                    transition hover:scale-105 duration-300"
+                >
+                    <p className="text-white">Try Now</p>
+                </button>
+            </div>
         </div>
     );
 }
