1c06bb9015d235a95ad8500ab09b8c89e5c4c37e
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-10-08 15:32:12 -0400

    favicon on root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OverPlan AI",
-  description: "Unlock Your Potential with AI-Driven Planning",
+  description: "Achieve More with AI-Driven Planning",
 };
 
 export default function RootLayout({
